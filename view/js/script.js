bacf52c6564379f197fe151051e60da8304d23d8
david toron <david@toron.popschool>
2017-01-21 17:36:34 +0100

    Indentation & optimisations

--- view/js/script.js
+++ view/js/script.js
@@ -1,9 +1,9 @@
 var lnStickyNavigation;
 
 $(document).ready(function()
-{	
+{
 	applyHeader();
-	applyNavigation(); 
+	applyNavigation();
 	applyMailTo();
 	applyResize();
 	checkHash();
@@ -15,22 +15,12 @@ $(document).ready(function()
 function applyHeader()
 {
 	$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	
+
 	lazyLoad($('.jumbotron'));
-}	
+}
 
 function lazyLoad(poContainer)
 {
-	/*var lstrSource   = poContainer.attr('data-src');
-	var lstrPosition = poContainer.attr('data-position');
-
-	$('<img>').attr('src', lstrSource).load(function()
-	{
-		poContainer.css('background-image', 'url("'+ lstrSource +'")');
-		poContainer.css('background-position', lstrPosition);
-		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
-		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
-	});*/
 }
 
 /* NAVIGATION FUNCTIONS */
@@ -48,7 +38,7 @@ function applyClickEvent()
 	$('a[href*=#]').on('click', function(e)
 	{
 		e.preventDefault();
-		
+
 		if( $( $.attr(this, 'href') ).length > 0 )
 		{
 			$('html, body').animate(
@@ -62,7 +52,7 @@ function applyClickEvent()
 
 function applyNavigationFixForPhone()
 {
-	$('.navbar li a').click(function(event) 
+	$('.navbar li a').click(function(event)
 	{
 		$('.navbar-collapse').removeClass('in').addClass('collapse');
 	});
@@ -70,7 +60,7 @@ function applyNavigationFixForPhone()
 
 function applyScrollSpy()
 {
-	$('#navbar-example').on('activate.bs.scrollspy', function() 
+	$('#navbar-example').on('activate.bs.scrollspy', function()
 	{
 		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
 	});
@@ -79,25 +69,25 @@ function applyScrollSpy()
 function applyStickyNavigation()
 {
 	lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-	$(window).on('scroll', function() 
-	{  
-		stickyNavigation();  
-	});  
-	
+
+	$(window).on('scroll', function()
+	{
+		stickyNavigation();
+	});
+
 	stickyNavigation();
 }
 
 function stickyNavigation()
-{         
-	if($(window).scrollTop() > lnStickyNavigation) 
-	{   
-		$('body').addClass('fixed');  
-	} 
-	else 
-	{  
-		$('body').removeClass('fixed');   
-	}  
+{
+	if($(window).scrollTop() > lnStickyNavigation)
+	{
+		$('body').addClass('fixed');
+	}
+	else
+	{
+		$('body').removeClass('fixed');
+	}
 }
 
 /* MAILTO FUNCTION */
@@ -107,9 +97,9 @@ function applyMailTo()
 	$('a[href*=mailto]').on('click', function(e)
 	{
 		var lstrEmail = $(this).attr('href').replace('mailto:', '');
-		
+
 		lstrEmail = lstrEmail.split('').reverse().join('')
-		
+
 		$(this).attr('href', 'mailto:' + lstrEmail);
 	});
 }
@@ -118,12 +108,12 @@ function applyMailTo()
 
 function applyResize()
 {
-	$(window).on('resize', function() 
-	{  
+	$(window).on('resize', function()
+	{
 		lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
+
 		$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	}); 
+	});
 }
 
 /* HASH FUNCTION */
@@ -131,74 +121,9 @@ function applyResize()
 function checkHash()
 {
 	lstrHash = window.location.hash.replace('#/', '#');
-	
+
 	if($('a[href='+ lstrHash +']').length > 0)
 	{
 		$('a[href='+ lstrHash +']').trigger('click');
 	}
 }
-
-/* IE7- FALLBACK FUNCTIONS */
-
-function checkBrowser()
-{
-	var loBrowserVersion = getBrowserAndVersion();
-	
-	if(loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8)
-	{ 
-		$('#upgrade-dialog').modal({
-			backdrop: 'static',
-			keyboard: false
-		});
-	}
-}
-
-function getBrowserAndVersion() 
-{
-	var laBrowserData = [{
-		string: 		navigator.userAgent,
-		subString: 		'MSIE',
-		identity: 		'Explorer',
-		versionSearch: 	'MSIE'
-	}];
-	
-	return {
-		browser: searchString(laBrowserData) || 'Modern Browser',
-		version: searchVersion(navigator.userAgent) || searchVersion(navigator.appVersion) || '0.0'
-	};
-}
-
-function searchString(paData) 
-{
-	for(var i = 0; i < paData.length; i++)	
-	{
-		var lstrDataString 	= paData[i].string;
-		var lstrDataProp 	= paData[i].prop;
-		
-		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
-		
-		if(lstrDataString) 
-		{
-			if(lstrDataString.indexOf(paData[i].subString) != -1)
-			{
-				return paData[i].identity;
-			}
-		}
-		else if(lstrDataProp)
-		{
-			return paData[i].identity;
-		}
-	}
-}
-	
-function searchVersion(pstrDataString) 
-{
-	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
-	
-	if(lnIndex == -1) 
-	{
-		return;
-	}
-	
-	return parseFloat(pstrDataString.substring(lnIndex + this.versionSearchString.length + 1));
-}	
